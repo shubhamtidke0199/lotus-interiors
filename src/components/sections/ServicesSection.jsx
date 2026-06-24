f8b333d8334f396d0b499d1a2d9787137c4d04b4
Shubham Tidke <shubhamtidke0199@gmail.com>
2026-06-25 00:19:36 +0530

    services responsiveness fix

--- src/components/sections/ServicesSection.jsx
+++ src/components/sections/ServicesSection.jsx
@@ -15,38 +15,32 @@ export default function ServicesSection() {
   return (
     <section
       aria-labelledby={titleId}
-      className="bg-white px-4 pb-16 pt-8 sm:px-6 lg:px-8 lg:pb-20"
+      className="overflow-x-hidden bg-white px-4 pb-16 pt-8 sm:px-6 lg:px-8 lg:pb-20"
     >
       <div className="mx-auto max-w-[1440px]">
         <ServicesSectionHeader titleId={titleId} />
 
-        <div className="flex flex-col gap-5">
-          <div className="grid grid-cols-1 gap-5 xl:grid-cols-[minmax(0,872fr)_minmax(0,496fr)] xl:grid-rows-[400px_330px] xl:gap-x-2">
+        <div className="flex flex-col gap-4 sm:gap-5">
+          <div className="grid grid-cols-1 gap-4 sm:gap-5 xl:grid-cols-[minmax(0,872fr)_minmax(0,496fr)] xl:grid-rows-[400px_330px] xl:gap-x-2">
             <ServiceCard
               {...residence}
-              className="h-[280px] sm:h-[360px] xl:col-start-1 xl:row-start-1 xl:h-[400px]"
+              className="h-[260px] sm:h-[360px] xl:col-start-1 xl:row-start-1 xl:h-[400px]"
             />
             <ServiceCard
               {...commercial}
               className="h-[280px] sm:h-[400px] xl:col-start-2 xl:row-span-2 xl:row-start-1 xl:h-[750px]"
             />
-            <div className="grid grid-cols-1 gap-[14px] sm:grid-cols-2 xl:col-start-1 xl:row-start-2">
-              <ServiceCard {...hospital} className="h-[280px] xl:h-[330px]" />
-              <ServiceCard
-                {...consultancy}
-                className="h-[280px] xl:h-[330px]"
-              />
+            <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 sm:gap-[14px] xl:col-start-1 xl:row-start-2">
+              <ServiceCard {...hospital} className="h-[240px] sm:h-[280px] xl:h-[330px]" />
+              <ServiceCard {...consultancy} className="h-[240px] sm:h-[280px] xl:h-[330px]" />
             </div>
           </div>
 
-          <div className="grid grid-cols-1 gap-3 md:grid-cols-2 md:gap-[12px]">
-            <ServiceCard
-              {...furniture}
-              className="h-[280px] xl:h-[330px]"
-            />
+          <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 sm:gap-[12px]">
+            <ServiceCard {...furniture} className="h-[260px] sm:h-[280px] xl:h-[330px]" />
             <ServiceCard
               {...projectManagement}
-              className="h-[280px] xl:h-[330px]"
+              className="h-[260px] sm:h-[280px] xl:h-[330px]"
             />
           </div>
         </div>
